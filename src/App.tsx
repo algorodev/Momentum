1a3ae38f6f71d7d6d05767700683ceab6260d790
Alex <me@algoro.dev>
2025-03-18 18:49:01 +0000

    feat: configured fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
-
 const App = () => (
-    <>
-      <p>Welcome to Momentum</p>
-    </>
-  )
+  <>
+	  <button className="bg-primary typography-md-regular text-white hover:bg-primary-light active:bg-primary-dark px-4 py-2 rounded">
+		  Get Started
+	  </button>
+	  <div className="text-secondary font-bold">Completed</div>
+	  <div className="bg-background text-text-primary min-h-screen">
+		  <h3>Welcome to Momentum</h3>
+	  </div>
+  </>
+)
 
 export default App
